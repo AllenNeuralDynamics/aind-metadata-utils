caca3189722e82a8de73203ad2dd42fede053bea
Dan Birman <danbirman@gmail.com>
2025-12-12 09:19:27 -0800

    fix: typo in metadata-portal URL

--- src/aind_metadata_utils/data_assets.py
+++ src/aind_metadata_utils/data_assets.py
@@ -8,7 +8,7 @@ def name_to_qc_view_link(asset_name: str) -> str:
 
 def name_to_metadata_view_link(asset_name: str) -> str:
     """Convert a data asset name to a Metadata view HTML link."""
-    return f'<a href="https://metadata.allenneuraldynamics-test.org/view?name={asset_name}" target="_blank">Metadata view</a>'
+    return f'<a href="https://metadata-portal.allenneuraldynamics-test.org/view?name={asset_name}" target="_blank">Metadata view</a>'
 
 
 def co_id_to_co_link(co_id: str) -> str:
